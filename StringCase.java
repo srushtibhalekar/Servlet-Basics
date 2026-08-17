774a97ecbcf7e32bc5dc15cbef2701aca1ed0ba5
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-17 22:23:18 +0530

    Add files via upload

---
+++ StringCase.java
@@ -0,0 +1,11 @@
+public class StringCase {
+
+    public static void main(String[] args) {
+
+        String name = "Srushti Bhalekar";
+
+        System.out.println("Original: " + name);
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+    }
+}
